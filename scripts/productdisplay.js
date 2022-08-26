4c8ccd479102c63c81767e5405242a3981c2c71e
Piryanshu88 <deepubisht2004@gmail.com>
2022-08-26 17:42:38 +0530

    adding product design page

---
+++ scripts/productdisplay.js
@@ -0,0 +1,358 @@
+let data=[{
+    "asin":"B08N5LLDSG",
+    "title":"2020 Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Silver",
+    "image":"https://m.media-amazon.com/images/I/71gD8WdSlaL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B08N5LLDSG/?psc=1",
+    "prices":{
+    "current_price":1199,
+    "previous_price":1299,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":5254,
+    "stars":4.8,
+    },
+    "prime":true,
+    "sponsored":true,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B08PPZMDPK",
+    "title":"MacBook Pro Apple M1 chip 8GB Memory 256GB SSD Space Gray",
+    "image":"https://m.media-amazon.com/images/I/71DIt6oIe3L._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B08PPZMDPK/?psc=1",
+    "prices":{
+    "current_price":940,
+    "previous_price":1199,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":39,
+    "stars":4.5,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B082J572X8",
+    "title":"Apple 13.3 MacBook Pro with Touch Bar, Intel Core i5 Quad-Core, 8GB RAM, 128GB SSD - Mid 2019, Space Gray",
+    "image":"https://m.media-amazon.com/images/I/71i49M4hv2L._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B082J572X8/?psc=1",
+    "prices":{
+    "current_price":819,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":768,
+    "stars":4.5,
+    },
+    "prime":true,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B07CN725RG",
+    "title":"Apple MacBook Pro MNQF2LL/A Intel i5-6287U X2 2.9GHz 8GB 512GB, Space Gray (Renewed)",
+    "image":"https://m.media-amazon.com/images/I/711RQIGWewL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B07CN725RG/?psc=1",
+    "prices":{
+    "current_price":679.99,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":25,
+    "stars":3.6,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B09JQML3NL",
+    "title":"2021 Apple MacBook Pro (16-inch, Apple M1 Pro chip with 10‑core CPU and 16‑core GPU, 16GB RAM, 1TB SSD) - Silver",
+    "image":"https://m.media-amazon.com/images/I/61Y30DpqRVL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B09JQML3NL/?psc=1",
+    "prices":{
+    "current_price":2699,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":135,
+    "stars":4.5,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B078GX9R5W",
+    "title":"Apple 13in MacBook Pro, Retina Display, 2.3GHz Intel Core i5 Dual Core, 8GB RAM, 128GB SSD, Space Grey, MPXQ2LL/A (Renewed)",
+    "image":"https://m.media-amazon.com/images/I/71nM55mRvxL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B078GX9R5W/?psc=1",
+    "prices":{
+    "current_price":519,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":1229,
+    "stars":4.1,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B077QWM132",
+    "title":"(Renewed) Apple MacBook Pro 13in Core i5 Retina 2.7GHz (MF840LL/A), 8GB Memory, 256GB Solid State Drive",
+    "image":"https://m.media-amazon.com/images/I/718L2WEaryL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B077QWM132/?psc=1",
+    "prices":{
+    "current_price":469.99,
+    "previous_price":499,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":1088,
+    "stars":4.3,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B09JQL8KP9",
+    "title":"2021 Apple MacBook Pro (14-inch, Apple M1 Pro chip with 8‑core CPU and 14‑core GPU, 16GB RAM, 512GB SSD) - Silver",
+    "image":"https://m.media-amazon.com/images/I/61cCf94xIEL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B09JQL8KP9/?psc=1",
+    "prices":{
+    "current_price":1999,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":143,
+    "stars":4.7,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B07ZFZ33SK",
+    "title":"Apple Macbook Pro MJLQ2LL/A 15-inch Laptop, Intel Core i7 Processor, 16GB RAM, 256GB SSD, Mac OS X (Renewed)",
+    "image":"https://m.media-amazon.com/images/I/81VbWDN53oL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B07ZFZ33SK/?psc=1",
+    "prices":{
+    "current_price":729.99,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":405,
+    "stars":4.3,
+    },
+    "prime":true,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B07MWJBFVM",
+    "title":"Apple MacBook Pro 13 MD101LL/A (4GB RAM, 500GB HD, macOS 10.13) - 1 Pack (Refurbished)",
+    "image":"https://m.media-amazon.com/images/I/71P25wQd49L._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B07MWJBFVM/?psc=1",
+    "prices":{
+    "current_price":324.72,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":366,
+    "stars":3.8,
+    },
+    "prime":false,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    },
+    {
+    "asin":"B0795941T3",
+    "title":"Apple MacBook Pro 13.3-Inch Laptop 2.6GHz (MGX72LL/A) Retina, 8GB Memory, 256GB Solid State Drive (Renewed)",
+    "image":"https://m.media-amazon.com/images/I/315CQ1KmlwL._AC_UY218_.jpg",
+    "full_link":"https://www.amazon.com/dp/B0795941T3/?psc=1",
+    "prices":{
+    "current_price":549,
+    "previous_price":-1,
+    "currency":"$",
+    },
+    "reviews":{
+    "total_reviews":1469,
+    "stars":4.2,
+    },
+    "prime":true,
+    "sponsored":false,
+    "amazon_choice":false,
+    "out_of_stock":false,
+    }
+]
+
+
+Appenddata(data)
+function Appenddata(data){
+    document.querySelector('#products_display').innerHTML=""
+
+    data.forEach((ele)=>{
+
+        let div=document.createElement('div')
+        div.style.display='flex'
+        div.style.justifyContent='space-evenly'
+
+        let div1=document.createElement('div')
+        div1.setAttribute('class','main_image')
+
+        let img=document.createElement('img')
+        img.setAttribute('src',ele.image)
+        img.style.width='90%'
+        div1.append(img)
+
+        let div2=document.createElement('div')
+        div2.style.lineHeight='35px'
+        div2.style.paddingLeft='15px'
+        let p=document.createElement('p')
+        p.innerText='sponsered'
+        p.style.color='grey'
+
+        let title=document.createElement('p')
+        title.innerText=ele.title
+        title.setAttribute('class','bo')
+
+
+        let model=document.createElement('p')
+        model.innerText=`Model : ${ele.asin}`
+
+        let div21=document.createElement('div')
+        div21.style.display='flex'
+        div21.style.height='40px'
+        div21.style.alignItems='center'
+
+        let ratinglogo=document.createElement('img')
+        ratinglogo.setAttribute('src','https://cdn4.iconfinder.com/data/icons/infographics-2-1/512/5_stars-128.png')
+        ratinglogo.style.width='20%'
+
+        let rating=document.createElement('p')
+        rating.innerText=`(${ele.reviews.total_reviews})`
+
+
+        div21.append(ratinglogo,rating)
+        let get=document.createElement('p')
+        get.innerText='Get it today'
+        get.style.color='green'
+
+        let div22=document.createElement('div')
+        div22.style.display='flex'
+        div22.style.alignItems='center'
+
+        let ship=document.createElement('img')
+        ship.setAttribute('src','https://cdn1.iconfinder.com/data/icons/lined-shipping/48/a-21-128.png')
+        ship.style.width='10%'
+
+        let ship_1=document.createElement('p')
+        ship_1.innerText='Pickup: Available today at Aiea'
+        div22.append(ship,ship_1)
+
+        let div23=document.createElement('div')
+        div23.style.display='flex'
+        div23.style.alignItems='center'
+
+        let area=document.createElement('img')
+        area.setAttribute('src','https://cdn1.iconfinder.com/data/icons/shopping-223/33/shopping-02-128.png')
+        area.style.width='10%'
+
+        let area_1=document.createElement('p')
+        area_1.innerText='Shipping: Unavailable in your area'
+        div23.append(area,area_1)
+
+        div2.append(p,title,model,get,div21,div22,div23)
+
+        let div3=document.createElement('div')
+        div3.style.lineHeight='30px'
+        div3.style.marginTop='-90px'
+        div3.style.paddingLeft='30px'
+        
+        let price=document.createElement('h2')
+        price.innerText=`$ ${ele.prices.current_price}`
+
+        let h3=document.createElement('h3')
+        h3.innerText="Save $300"
+        h3.style.paddingLeft='10px'
+        h3.style.marginRight='210px'
+        h3.style.color='white'
+        h3.style.backgroundColor='#BB0628'
+        h3.style.display='block'
+
+        let esehi=document.createElement('p')
+        esehi.setAttribute('class','ese')
+        esehi.innerText='Free 6-month security software'
+
+        let addtocart=document.createElement('button')
+        addtocart.innerText='Add to cart'
+        addtocart.setAttribute('class','btn')
+
+        addtocart.addEventListener('click',function(){
+
+            console.log('bahi')
+            Addtocart(ele)
+
+        })
+
+        div3.append(price,h3,esehi,addtocart)
+
+        div.append(div1,div2,div3)
+
+        document.querySelector('#products_display').append(div)
+    })
+}
+
+
+function Addtocart(ele){
+    let arr=JSON.parse(localStorage.getItem('add-to-cart'))||[]
+    arr.push(ele)
+    localStorage.setItem('add-to-cart',JSON.stringify(arr))
+
+}
+
+// arr_of_video.sort(function(a,b){
+//     let x=a.snippet.title.toUpperCase()
+//     let y=b.snippet.title.toLowerCase()
+//     if(x>y){
+//         return 1
+//     }
+//     else if(x<y){
+//         return -1
+//     }
+//     else{
+//         return 0
+//     }
+// })
+
+// for(let i=0;i<data.length;i++){
+//     let {prices}=data[i]
+//     prices.current_price.sort(function(a,b){
+//         return a-b
+//     })
+// }
+// console.log(data)
+
